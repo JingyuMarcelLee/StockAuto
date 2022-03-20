d2d72e00c2db55509f3c693576a9d074af7fd1af
Jingyu (Marcel) Lee <46755208+JingyuMarcelLee@users.noreply.github.com>
2022-03-20 17:53:04 -0400

    Update autotrade.py

--- autotrade.py
+++ autotrade.py
@@ -12,7 +12,7 @@ def post_message(token, channel, text):
         data={"channel": channel,"text": text}
     )
  
-myToken = "xoxb-3031085882624-3267050540002-QTFXuyz9RM4AlBITRrQPosDf"
+myToken = "token token"
 def dbgout(message):
     """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
     print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
