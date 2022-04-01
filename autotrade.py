35c07ecfa13ce7841669b56e488c6881bc7299a3
Jingyu (Marcel) Lee <46755208+JingyuMarcelLee@users.noreply.github.com>
2022-03-31 21:22:12 -0400

    Update autotrade.py

--- autotrade.py
+++ autotrade.py
@@ -63,12 +63,12 @@ def get_current_price(code):
 
 def get_ohlc(code, qty):
     """return OHLC price info based on the quantity (qty)"""
-    cpOhlc.SetInputValue(0, code)           # 종목코드
-    cpOhlc.SetInputValue(1, ord('2'))        # 1:기간, 2:개수
-    cpOhlc.SetInputValue(4, qty)             # 요청개수
-    cpOhlc.SetInputValue(5, [0, 2, 3, 4, 5]) # 0:날짜, 2~5:OHLC
-    cpOhlc.SetInputValue(6, ord('D'))        # D:일단위
-    cpOhlc.SetInputValue(9, ord('1'))        # 0:무수정주가, 1:수정주가
+    cpOhlc.SetInputValue(0, code)           # code
+    cpOhlc.SetInputValue(1, ord('2'))        # 1:period, 2:qty
+    cpOhlc.SetInputValue(4, qty)             # order qty
+    cpOhlc.SetInputValue(5, [0, 2, 3, 4, 5]) # 0:date, 2~5:OHLC
+    cpOhlc.SetInputValue(6, ord('D'))        # D:day
+    cpOhlc.SetInputValue(9, ord('1'))        # 0:share value, 1: value after alteration
     cpOhlc.BlockRequest()
     count = cpOhlc.GetHeaderValue(3)   # 3:수신개수
     columns = ['open', 'high', 'low', 'close']
